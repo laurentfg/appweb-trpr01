0369ad54227321b4d1beba73dd9c896f3f628795
laurentfg <laurent_ferland_girouard@hotmail.com>
2025-03-10 00:13:54 -0400

    correction du fichier de config pour que ça soit compatible sur la page web

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
+base: "/appweb-trpr01/"
 export default defineConfig({
   plugins: [vue()],
 
